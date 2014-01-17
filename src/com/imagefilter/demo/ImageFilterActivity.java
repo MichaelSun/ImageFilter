6fda3de4a1fb70d6f87af2f233a7b7a9970543a3
guoqing.sun <guoqing.sun@renren-inc.com>
2014-01-17 18:42:02 +0800

    create project

---
+++ src/com/imagefilter/demo/ImageFilterActivity.java
@@ -0,0 +1,15 @@
+package com.imagefilter.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageFilterActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
